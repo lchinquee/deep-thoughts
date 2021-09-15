bad31da830d8a63ee64467b5fc094f72aa174e14
LChinQuee <81491306+lchinquee@users.noreply.github.com>
2021-09-14 23:24:32 -0400

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,27 +11,26 @@ import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 
-
-const httpLink = createHttpLink({
-  uri: 'graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
-
 function App() {
+  const httpLink = createHttpLink({
+    uri: 'graphql',
+  });
+  
+  const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('id_token');
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
+  
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+  
   return (
     <ApolloProvider client={client}>
       <Router>
